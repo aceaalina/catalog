f41b50ba040fc91a70676a04d9736d533eb49518
aceaalina <acea.alina@gmail.com>
2024-06-30 18:00:56 +0300

    Implemented actions for viewing and deleting student records and their grades
    Added functionality to show student grades upon button click
    Implemented deletion of students and their respective grades
    Added ability to hide the grade section when desired

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ const elevi = [
   { nume: "Alexandru Dumitrescu", note: [7, 8, 6], medie: (7 + 8 + 6) / 3 },
   { nume: "Ioana Georgescu", note: [9, 8, 9], medie: (9 + 8 + 9) / 3 },
   { nume: "Mihai Marinescu", note: [6, 7, 8], medie: (6 + 7 + 8) / 3 },
-  ,
 ];
 
 const inputNumeElev = document.getElementById("nume-elev-input");
 const butonAdaugareElev = document.getElementById("adauga-elev-btn");
 const tabelElevi = document.getElementById("tabel-elevi");
+const tabelNote = document.getElementById("tabel-note");
 const sectiuneNote = document.getElementById("note-elev-wrapper");
-const butonAscundeNote = document.getElementById("ascunde-note");
+const butonAscundereNote = document.getElementById("ascunde-note");
+const containerNoteElev = document.getElementById("note-elev-wrapper");
 
 afisareTabel(elevi);
 
 butonAdaugareElev.addEventListener("click", adaugareElevInTabel);
-tabelElevi.addEventListener("click", trateazaActiuniTabel);
-butonAscundeNote.addEventListener("click", ascundeSectiuneNote);
+tabelElevi.addEventListener("click", trateazaActiuniTabelElevi);
+tabelNote.addEventListener("click", trateazaActiuniTabeNote);
+butonAscundereNote.addEventListener("click", ascundeSectiuneNote);
 
 function adaugareElevInTabel() {
   const numeElev = inputNumeElev.value;
@@ -35,22 +37,63 @@ function afisareTabel(elevi) {
 
   for (let i = 0; i <= elevi.length - 1; i++) {
     tableBody.innerHTML += `
-        <tr>
+         <tr id="elev_${i}">
             <td>${elevi[i].nume}</td>
             <td>${elevi[i].medie.toFixed(2)}</td>
             <td><button class="vezi-note">Vezi Notele</button></td>
-            <td><button class="sterge-elevi">X</button></td>
-        </tr>
-    `;
+            <td><button class="sterge-elev">X</button></td>
+         </tr>
+      `;
   }
 }
 
-function trateazaActiuniTabel(e) {
+function trateazaActiuniTabelElevi(e) {
   if (e.target.classList.contains("vezi-note")) {
     sectiuneNote.classList.remove("hide");
+    const id = e.target.parentElement.parentElement.id;
+    const index = id.split("_")[1];
+    afiseazaNote(elevi[index]);
+  } else if (e.target.classList.contains("sterge-elev")) {
+    const id = e.target.parentElement.parentElement.id;
+    const index = id.split("_")[1];
+
+    elevi.splice(index, 1);
+    afisareTabel(elevi);
+  }
+}
+
+function trateazaActiuniTabeNote(e) {
+  if (e.target.classList.contains("sterge-nota")) {
+    const idNota = e.target.parentElement.parentElement.id;
+    const indexNota = idNota.split("_")[1];
+
+    const idTableBody = e.target.parentElement.parentElement.parentElement.id;
+    const indexElev = idTableBody.split("_")[1];
+
+    elevi[indexElev].note.splice(indexNota, 1);
+    afiseazaNote(elevi[indexElev]);
   }
 }
 
 function ascundeSectiuneNote() {
   sectiuneNote.classList.add("hide");
+}
+
+function afiseazaNote(elev) {
+  const index = elevi.indexOf(elev);
+  const h1NumeElev = containerNoteElev.querySelector("h1");
+  h1NumeElev.innerHTML = elev.nume;
+  const tableBody = tabelNote.querySelector("tbody");
+
+  tableBody.innerHTML = "";
+
+  for (let i = 0; i <= elev.note.length - 1; i++) {
+    tableBody.id = `elev_${index}`;
+    tableBody.innerHTML += ` 
+        <tr id="nota_${i}">
+            <td>${elev.note[i].toFixed(2)}</td>
+            <td><button class="sterge-nota">X</button></td>
+        </tr>
+    `;
+  }
 }
